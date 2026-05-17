37c0c0f5584b66cac2195d842752875eb615b7d7
saguayaquil02 <saguayaquil02@gmail.com>
2026-05-17 09:39:03 -0500

    Control de ventas - agregar funcion para registrar los productos

--- Control_Ventas/control_ventas.cpp
+++ Control_Ventas/control_ventas.cpp
@@ -76,7 +76,7 @@ Fin
 */
 
 #include <iostream>
-#include <string>
+#include <string> // agregamos la libreria para usar cadena de texto
 using namespace std;
 
 // Constante global para el tamaño máximo de los arreglos
@@ -90,7 +90,7 @@ void mostrarMenu();
 int validarOpcion(int min, int max);
 
 // Permitir ingresar productos al sistema
-void registrarProductos();
+void registrarProductos(string nombres[], float precios[], int stock[], int &cantidadActual);
 
 // Mostrar la lista de productos en formato de tabla
 void mostrarInventario();
@@ -100,7 +100,45 @@ void realizarVenta();
 
 // Funcion principal
 int main(){
+    // Declaramos los arreglos para la base de datos de la tienda
+    string nombresProductos[N]; // cadena de texto
+    float preciosProductos[N];
+    int stockProductos[N];
+    
+    // Variables de control
+    int cantidadActual = 0; // Cuantos productos se han registrado realmente
+    float totalCaja = 0.0;  // Acumulador de dinero recaudado
+    int opcion;
 
+    do {
+        mostrarMenu();
+        cout << "Seleccione una opcion: ";
+        opcion = validarOpcion(1, 5); // Valida que solo elija entre 1 y 5
+
+        switch (opcion) {
+            case 1:
+                cout << "\n--- REGISTRO DE PRODUCTOS ---" << endl;
+                // Llamamos a registrarProductos
+                break;
+            case 2:
+                cout << "\n--- INVENTARIO ACTUAL ---" << endl;
+                // Llamamos a mostrarInventario
+                break;
+            case 3:
+                cout << "\n--- NUEVA VENTA ---" << endl;
+                // Llamamos a realizarVenta
+                break;
+            case 4:
+                cout << "\n--- CIERRE DE CAJA ---" << endl;
+                cout << "Total recaudado el dia de hoy: $" << totalCaja << endl;
+                break;
+            case 5:
+                cout << "\n¡Gracias por usar el sistema! Saliendo..." << endl;
+                break;
+        }
+    } while (opcion != 5);
+
+    return 0;
 }
 
 // Funciones
@@ -125,4 +163,33 @@ int validarOpcion(int min, int max) {
         cin >> entrada; // Volver a pedir el dato
     }
     return entrada;
+}
+
+// Funcion para crear una base de datos para registar los productos
+void registrarProductos(string nombres[], float precios[], int stock[], int &cantidadActual) {
+    if (cantidadActual >= N) {
+        cout << "Error: La base de datos esta llena.\n";
+        return; 
+    }
+
+    // Leemos el string
+    cout << "Ingrese el nombre (use guion bajo para espacios, ej: papa_frita): ";
+    cin >> nombres[cantidadActual];
+
+    float precio;
+    cout << "Ingrese el precio del producto: $";
+    cin >> precio;
+    
+    // Validación de precio con un while 
+    while (precio <= 0) {
+        cout << "Error: El precio debe ser mayor a 0. Intente de nuevo: $";
+        cin >> precio;
+    }
+    precios[cantidadActual] = precio;
+
+    cout << "Ingrese el stock inicial: ";
+    stock[cantidadActual] = validarOpcion(0, 10000); // podemos usar la funcion para validar opcion en el rango que queramos
+
+    cantidadActual++;
+    cout << "¡Producto registrado exitosamente!" << endl;
 }
