186f0d05b4602037d9c4e852ff719d109e37acfe
saguayaquil02 <saguayaquil02@gmail.com>
2026-05-17 01:08:41 -0500

    Control de Ventas - crear estructura inicial

---
+++ Control_Ventas/control_ventas.cpp
@@ -0,0 +1,89 @@
+/* 
+Bootcamp Programación Cero a POO 
+
+Mini proyecto en C++ - Tema: Sistema de control de ventas
+
+Autor: Sebastian Guayaquil
+Fecha: 15 de mayo del 2026
+
+Enunciado: Desarrollar un sistema de control de ventas.
+El sistema debe realizar operaciones con la información almacenada:
+    • cálculo
+    • conteos
+    • búsquedas
+    • promedios
+    • totales
+    • máximos y mínimos
+    • reportes simples
+
+E/P/S
+
+Entrada:
+    Menu: una opcion.
+    Gestión de Productos: nombre, precio, stock del producto.
+    Venta tipo de producto y cantidad a vender.
+
+Proceso:
+    Inicialización: Definir el tamaño máximo de los arreglos y declarar las variables para el control de caja.
+    Validación de Datos.
+    Verificar que los precios y cantidades sean mayores a cero.
+    Realizar los calulos, conteos, busquedas.
+    Búsqueda: Localizar el producto en los arreglos.
+    Cálculo de Ventas
+        subtotal
+        total
+        total de caja
+        actualizacion de inventario
+
+Salida:
+    Mostrar un menu
+    Alertas y errores
+    Reportes de inventario
+    Factura o recibo
+    Cierre de caja
+*/
+
+/*
+PSEUDOCÓDIGO
+
+Algoritmo ControlVentas
+    Definir N como entero 
+    Dimension nombresProductos[N];
+    Dimension arreglo preciosProductos[N];
+    Dimension stockProductos[N];
+
+    Definir cantidadActual como Entero
+    Definir totalCaja como Real
+    Definir opcion como Entero
+
+    Funcion mostrarMenu()
+    Funcion validarOpcion()
+    Funcion registrarProductos()
+    Funcion mostrarInventario()
+    Funcion realizarVenta()
+
+
+FinAlgoritmo
+
+Inicio
+    mostrarMenu(...)
+    validarOpcion(...)
+    registrarProductos(...)
+    mostrarInventario(...)
+    realizarVenta(...)
+
+Fin
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Prototipo de las funiones
+
+// Funcion principal
+int main(){
+
+}
+
+// Funciones
